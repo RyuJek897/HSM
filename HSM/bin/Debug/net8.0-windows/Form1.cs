ffa0c828b34a6e9791b426e97baf26a5678a53d9
RyuJek897 <125415375+RyuJek897@users.noreply.github.com>
2024-04-22 22:14:54 +0700

    HSM 2

---
+++ HSM/bin/Debug/net8.0-windows/Form1.cs
@@ -0,0 +1,25 @@
+namespace HardwareShopManagement1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            panel2.Visible = true;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
